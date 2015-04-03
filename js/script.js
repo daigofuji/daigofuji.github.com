2d3a331fc5dd6fb3bfef30a84d2c6daada479fa2
Daigo Fujiwara <daigo@daigofujiwara.com>
2015-04-03 01:29:48 -0400

    Site updated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+$(document).foundation().ready(function() {
+  $('#js-fullpage').fullpage();
+
+  $(".slide2 .rotate").textrotator({
+    animation: "fade",
+    speed: 1250
+  });
+});
+
