2922bac581b41e1e125a1631e288f7d745cf7ed6
Daigo Fujiwara <daigo@daigofujiwara.com>
2015-04-03 02:12:58 -0400

    fullpage options added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation().ready(function() {
-  $('#js-fullpage').fullpage();
+  $('#js-fullpage').fullpage({
+    loopBottom: true,
+    loopTop: true,
+    anchors:['slide1', 'slide2', 'slide3', 'slide4', 'slide5', 'slide6', 'slide7']
+  });
 
   $(".slide2 .rotate").textrotator({
     animation: "fade",
