cbfd60e9f91c924389bed56df81e138d39eb2873
Daigo Fujiwara <daigo@daigofujiwara.com>
2015-04-11 00:31:19 -0400

    Not using fullpage js

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ daigofuji.github.com
 
 DaigoFuji's github page
 
+To run server `bundle exec jekyll serve --watch`
+
+To run grunt watch for css `grunt`
--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation().ready(function() {
-  $('#js-fullpage').fullpage({
-    loopBottom: true,
-    loopTop: true,
-    scrollOverflow:true,
-    anchors:['hello', 'what-i-do', 'baseball', 'talks', 'baseballhackday', 'crafts', 'contact']
-  });
+  // $('.js-fullpage').fullpage({
+  //   loopBottom: true,
+  //   loopTop: true,
+  //   anchors:['hello', 'what-i-do', 'baseball', 'talks', 'baseballhackday', 'crafts', 'contact']
+  // });
 
   $(".slide2 .rotate").textrotator({
     animation: "fade", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
