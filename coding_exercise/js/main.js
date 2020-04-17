f585e4464d6351691c21739a1c1b12783c29eb65
Daigo Fujiwara <daigo@daigofujiwara.com>
2020-04-17 09:43:14 -0400

    add slideout

--- coding_exercise/js/main.js
+++ coding_exercise/js/main.js
@@ -1,20 +1,29 @@
-const menu = document.querySelector('#open-modal');
-menu.addEventListener('click', openModal);
+const menu = document.querySelector('#open-menu');
+menu.addEventListener('click', openMenu);
+
+const nav = document.querySelector('#open-modal');
+nav.addEventListener('click', openModal);
 
 const modal = document.querySelector('#modal');
 
 const btn = document.querySelector('#show-result');
 btn.addEventListener('click', loadResult);
 
+function openMenu(ev) {
+  document.querySelector('#slideout').classList.toggle('open');
+}
+
 function openModal(ev) {
   modal.style.display = 'block';
+  document.querySelector('#slideout').classList.remove('open');
+
 }
 
 function loadResult(ev) {
   ev.target.remove();
   const loader = document.querySelector('#loading');
   loader.style.display = 'block';
-
+  // simulate ajax call
   setTimeout(function() {
     loader.remove();
     let result = document.createElement('p');
